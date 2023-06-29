25f3f6ea738c18f4386d50a8c1b3b6fded1e8a48
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-29 18:59:00 -0300

    Home made update

--- desktop_app/src/components/Dashboard/Tabs/Home/HomePage.tsx
+++ desktop_app/src/components/Dashboard/Tabs/Home/HomePage.tsx
@@ -1,7 +1,13 @@
 export default function HomePage() {
     return (
-        <div className='p-4'>
-            <h2 className='text-2xl font-bold'>Home </h2>
+        <div className='p-4 flex justify-center items-center h-full'>
+            <div className="flex flex-col justify-center text-center align-middle">
+                <img src="src/data/img/Logo-HandSpeak.png" alt="home gif" className="homeImage w-36 animate-spin mx-auto" />
+                <h1 className='homeTitle font-bold'>HANDSPEAK</h1>
+                <p className='text-2xl font-normal'>Convierte tus manos en palabras con nuestra app de iterpretación
+                    de lenguaje de señas impulsada por inteligencia artificial
+                </p>
+            </div>
         </div>
     )
 }
