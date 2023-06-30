2e1d446678f7a61277c9f49e85afed04189d5f7c
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-06-29 23:32:47 -0300

    Dashboard animations

--- desktop_app/src/components/Dashboard/Dashboard.tsx
+++ desktop_app/src/components/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
 import { dominantHandType } from "@/types/dataTypes";
 import { selectedTabType } from "@/types/dataTypes";
 
@@ -9,77 +9,76 @@ export default function Dashboard({
   selectedTab: string;
   handleSelectTab: (value: selectedTabType) => void;
 }) {
-  const [dominantHand, setDominantHand] = useState<dominantHandType>("derecha");
+  const [dominantHand, setDominantHand] = useState<dominantHandType>('derecha');
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animationStyle, setAnimationStyle] = useState({ top: 0 });
+
+  useEffect(() => {
+    const tabs = document.querySelectorAll('.tabs li');
+    const activeTab = tabs[activeIndex];
+    const activeTabTop = activeTab.offsetTop;
+
+    setAnimationStyle({ top: activeTabTop });
+  }, [activeIndex]);
+
+  const handleSelectTabWithAnimation = (tab: string, index: number) => {
+    setActiveIndex(index);
+    handleSelectTab(tab);
+  };
 
   return (
     <div className="flex flex-col justify-between basis-1/6 h-full bg-base-200">
       <div>
         <ul className="tabs tabs-boxed flex-col items-stretch drop-shadow-lg">
           <li
-            onClick={() => {
-              handleSelectTab("home");
-            }}
+            onClick={() => handleSelectTabWithAnimation('home', 0)}
+            className={`stat ${selectedTab === 'home' ? 'dashTags' : ''}`}
           >
-            <a
-              href="#"
-              className={`stat ${selectedTab === "home" ? "tab-active" : ""}`}
-            >
+            <a href="#" className={activeIndex === 0 ? 'animate-color-transition text-xl font-semibold' : 'text-xl font-semibold'}>
               Home
             </a>
           </li>
           <li
-            onClick={() => {
-              handleSelectTab("educacion");
-            }}
+            onClick={() => handleSelectTabWithAnimation('educacion', 1)}
+            className={`stat ${
+              selectedTab === 'educacion' || selectedTab === 'clases' ? 'dashTags text-xl font-semibold' : 'text-xl font-semibold'
+            }`}
           >
-            <a
-              href="#"
-              className={`stat ${
-                selectedTab === "educacion" || selectedTab === "clases"
-                  ? "tab-active"
-                  : ""
-              }`}
-            >
+            <a href="#" className={activeIndex === 1 ? 'animate-color-transition text-xl font-semibold' : 'text-xl font-semibold'}>
               Educación
             </a>
           </li>
           <li
-            onClick={() => {
-              handleSelectTab("practica");
-            }}
+            onClick={() => handleSelectTabWithAnimation('practica', 2)}
+            className={`stat ${selectedTab === 'practica' ? 'dashTags' : ''}`}
           >
-            <a
-              href="#"
-              className={`stat ${
-                selectedTab === "practica" ? "tab-active" : ""
-              }`}
-            >
+            <a href="#" className={activeIndex === 2 ? 'text-xl animate-color-transition font-semibold' : 'text-xl font-semibold'}>
               Práctica
             </a>
           </li>
           <li
-            onClick={() => {
-              handleSelectTab("glosario");
-            }}
+            onClick={() => handleSelectTabWithAnimation('glosario', 3)}
+            className={`stat ${selectedTab === 'glosario' ? 'dashTags' : ''}`}
           >
-            <a
-              href="#"
-              className={`stat ${
-                selectedTab === "glosario" ? "tab-active" : ""
-              }`}
-            >
+            <a href="#" className={activeIndex === 3 ? 'animate-color-transition text-xl font-semibold' : 'text-xl font-semibold'}>
               Glosario
             </a>
           </li>
+          <div
+            className="tab-indicator"
+            style={{
+              top: animationStyle.top,
+              transition: 'top 0.3s ease',
+            }}
+          />
         </ul>
       </div>
       <div className="flex flex-col p-4 border-primary border-[1.5px] m-4 items-center gap-4 rounded-box">
         <span>Mano dominante</span>
         <div className="btn-group">
           <button
-            className={`btn ${
-              dominantHand === "izquierda" ? "btn-active" : ""
-            }`}
+            className={`btn ${dominantHand === "izquierda" ? "btn-active" : ""
+              }`}
             onClick={() => {
               setDominantHand("izquierda");
             }}
