61121c11a62cb9ea45ae7f1529955fab630dd96d
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-07-02 01:53:24 -0300

    size windows

--- desktop_app/electron/main.ts
+++ desktop_app/electron/main.ts
@@ -26,12 +26,14 @@ const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 function createWindow() {
 
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    minWidth: 1000,
+    minHeight: 500,
+     // nodeIntegration: true,
     icon: path.join(process.env.PUBLIC, 'electron-vite.svg'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
+    // autoHideMenuBar: true,
     // fullscreen: true
   })
 
