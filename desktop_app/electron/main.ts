b108249e7d178753c10213014a19226123e1be87
Sapitorico <105575956+Sapitorico@users.noreply.github.com>
2023-07-02 13:51:28 -0300

    fix data types

--- desktop_app/electron/main.ts
+++ desktop_app/electron/main.ts
@@ -1,72 +1,60 @@
-import { app, BrowserWindow } from 'electron'
-import path from 'node:path'
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
 import { spawn } from 'child_process';
-import kill from 'tree-kill';
+import treeKill from 'tree-kill';
 
-let terminal 
-let isTerminating = false
+let terminal: any;
+let isTerminating = false;
 
-// The built directory structure
-//
-// ├─┬─┬ dist
-// │ │ └── index.html
-// │ │
-// │ ├─┬ dist-electron
-// │ │ ├── main.js
-// │ │ └── preload.js
-// │
-process.env.DIST = path.join(__dirname, '../dist')
-process.env.PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+process.env.DIST = path.join(__dirname, '../dist');
+process.env.PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public');
 
-
-let win: BrowserWindow | null
-// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+let win: BrowserWindow | null = null;
 
 function createWindow() {
-
   win = new BrowserWindow({
-    minWidth: 1000,
+    minWidth: 768,
     minHeight: 500,
-     // nodeIntegration: true,
     icon: path.join(process.env.PUBLIC, 'electron-vite.svg'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
     },
-    // autoHideMenuBar: true,
-    // fullscreen: true
-  })
+  });
 
-  // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+    win?.webContents.send('main-process-message', new Date().toLocaleString());
+  });
 
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
   } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(process.env.DIST, 'index.html'))
+    win.loadFile(path.join(process.env.DIST, 'index.html'));
   }
 }
 
 app.whenReady().then(() => {
- const directoryPath = path.join(__dirname, '../../backend_api/modules')
+  const directoryPath = path.join(__dirname, '../../backend_api/modules');
   const command = 'uvicorn main:app';
-  terminal = spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+
+  const shell = process.platform === 'win32' ? 'cmd.exe' : 'bash';
+  const shellArgs = process.platform === 'win32' ? ['/c'] : ['-c'];
+
+  terminal = spawn(shell, [...shellArgs, command], {
     cwd: directoryPath,
     shell: true,
+    stdio: 'ignore',
   });
-  
-  terminal.stdin.write(`${command}\n`)
-  createWindow()
-})
+
+  createWindow();
+});
 
 app.on('before-quit', (event) => {
   if (!isTerminating) {
     event.preventDefault();
     isTerminating = true;
-    kill(terminal.pid, 'SIGTERM', (err) => {
+
+    treeKill(terminal.pid, 'SIGTERM', (err) => {
       if (err) {
         console.error('Failed to kill terminal process:', err);
         isTerminating = false;
@@ -78,5 +66,7 @@ app.on('before-quit', (event) => {
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
